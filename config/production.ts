2e9febc1ee868aab28927e84e19579104f839e6e
brij mohan singh <brijsingh100685@gmail.com>
2022-06-07 13:06:58 +0530

    Update production.ts

--- config/production.ts
+++ config/production.ts
@@ -1,1 +1,10 @@
-export default {};
+export default {port: 1337,
+  dbUri: "mongodb+srv://new-test-1:BbBb100685@cluster0.jalkq.mongodb.net/Digikraft",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+ };
